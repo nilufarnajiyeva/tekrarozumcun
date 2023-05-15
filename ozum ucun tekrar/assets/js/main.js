4719f93dfb683b351de17ce6c5c7d04e8613a703
Nilufar Najiyeva <127186140+nilufarnajiyeva@users.noreply.github.com>
2023-05-15 10:21:04 +0400

    Add files via upload

---
+++ ozum ucun tekrar/assets/js/main.js
@@ -0,0 +1,33 @@
+// Proqram ikirəqəmli ədədi oxuyur və ədədin hər bir rəqəmini arada boşluq olmaqla ekrana verir.
+
+
+// var  a=prompt("eded daxil edin");
+// if(a>=10 &&a<=99){
+//  var teklik=a%10
+//  var onluq = (a-teklik)/10
+
+// }
+// alert(onluq+" "+ teklik)
+  
+
+let result = '';
+let i = 0;
+
+do {
+  i = i + 1;
+  result = result + i;
+} while (i < 5);
+console.log(result);
+ 
+
+let n = 0;
+
+while (n < 3) {
+ n++;
+ 
+}
+console.log(n);
+
+
+
+
